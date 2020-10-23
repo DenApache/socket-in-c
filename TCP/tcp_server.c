801486e541a72ef9c141dfbbc6500acba02e402e
vince-stri <acilav.univtlse@gmail.com>
2020-10-23 09:57:22 +0200

    TCP server/client over with param and verif

--- TCP/tcp_server.c
+++ TCP/tcp_server.c
@@ -1,22 +1,14 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<sys/types.h>
-// #include<sys/socket.h>
-// #include<netinet/in.h>
-// #include<unistd.h>
 #include "../utils.h"
 
 
 int main(int argc, char** argv){
-    
-    
-    //checks the validity of the arguments     
-    check_args_server(argc, argv);
 
+    //checks the validity of the arguments
+    check_args_server(argc, argv);
     PORT port = atoi(argv[1]);
     SOCKET server_socket;
     SOCKET client_socket;
-    char server_message[256] = "You have reached the server \n";
+    char client_message[256];
 
     //create the server socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,19 +19,41 @@ int main(int argc, char** argv){
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
+    //define client socket
+    struct sockaddr_in new_client;
+    socklen_t new_client_size = sizeof(new_client);
+
     display_log();
     printf("TCP Server starts\n");
-    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    display_log();
-    printf("Listening...\n");
-    listen(server_socket, 5);
-    while (1){
-        client_socket = accept(server_socket, NULL, NULL);    
-        display_log();
-        printf("New client in \n");
-        send(client_socket, server_message, sizeof(server_message),0);
-    }
 
+    //Bin the socket
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    //Listen on port $port
+    listen(server_socket, 5);
+    display_log();
+    printf("Listening on port %d...\n", port);
+
+    while ((client_socket = accept(server_socket, (struct sockaddr *) &new_client, &new_client_size)) > 0 ){
+        //Get Hostname from the client
+        char hostname[64];
+        bzero(hostname, 64);
+        getnameinfo((struct sockaddr *) &new_client, new_client_size, hostname, 64-1, NULL, 0,0);
+
+        //New connection from a client
+        display_log();
+        printf("New connection from %s : %s\n", inet_ntoa(new_client.sin_addr),hostname);
+        recv(client_socket,client_message, sizeof(client_message),0);
+        
+        //Display client message
+        display_log();
+        printf("Receive from %s (%s) : %s\n", inet_ntoa(new_client.sin_addr),hostname, client_message);
+
+        //Sends the message receveid from the client
+        display_log();
+        printf("Sent to %s (%s) : %s\n", inet_ntoa(new_client.sin_addr),hostname, client_message);
+        send(client_socket, client_message, sizeof(client_message),0);
+    }
     close(server_socket);
     return 0;
 }
--- utils.c
+++ utils.c
@@ -11,7 +11,7 @@ void check_port(char* sport){
     //Checks port's interval
     int port = atoi(sport);
     if (!(port>1024) || !(port<=65535)){
-        printf("The TCP port must be included between 2014 and 65535\n");
+        printf("The port must be included between 2014 and 65535\n");
         exit(1);
     }
 }
@@ -26,14 +26,76 @@ void check_args_server(int n, char** args){
     check_port(args[1]);
 }
 
-int check_args_client(int n, char** args){
+int check_server_address(char* IP ){
+    int validity, byte, IP_len, dotCount;
+       
+    validity = 1;
+    IP_len = strlen(IP);
+    dotCount = 0;
+   
+    // Checks the length of the chain
+    if( !IP_len || IP_len > 15)
+    {
+        validity = 0;
+    }
+    else
+    {
+        /* We recover the first byte  */
+        byte = atoi(IP);
+        
+        /* If it is greater than 255 then it is not a valid IP */
+        if(byte > 255)
+        {
+            validity = 0;
+        }
+           
+        /* We go through the chain */
+        for(int i = 0; i < IP_len && validity; i++)
+        {
+            /* We check if each character is only a number or a dot */
+            if(!(IP[i] >= '0' && IP[i] <= '9') && IP[i] != '.')
+            {
+                validity = 0;
+            }
+          
+            /* Count the number of dots*/
+            if(IP[i] == '.')
+            {
+                dotCount ++;
+                /* We check if the byte we retrieve is valid */
+                byte = atoi(&IP[i+1]);
+                if(byte > 255)
+                {
+                    validity = 0;
+                }
+            }
+        }
+        /* if there are not three points in the ip then it is not a valid ip */
+        if(dotCount != 3)
+        {
+            validity = 0;
+        }
+           
+    }
+       
+   
+    return validity; 
+}
+
+void check_args_client(int n, char** args){
     if (n != 4){
-        printf("USAGE : %s ADRESSE PORT MESSAGE\n", args[0] );
+        printf("USAGE : %s SERVER_ADDRESS PORT MESSAGE\n", args[0] );
         exit(0);
     }
     check_port(args[2]);
-    
-    return 0;
+    if (!check_server_address(args[1])){
+        printf("Address IP is not valid\n");
+        exit(1);
+    }
+    if (strlen(args[3])>=256){
+        printf("Size must not exceed 255 bytes\n");
+        exit(2);
+    }
 }
 
 void display_log(){
